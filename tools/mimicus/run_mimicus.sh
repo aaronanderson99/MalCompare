71e75cd8882666324bd58e259ed12e6ad7722a0f
GateGuy <gateguy33@gmail.com>
2020-11-24 22:16:16 -0500

    Updated Mimicus shell scripts

--- tools/mimicus/run_mimicus.sh
+++ tools/mimicus/run_mimicus.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Note: this script assumes that you are currently in the mimicus directory (where this script is located)
-
 test_dir=$1
 
 if [ ! -d $test_dir ]
@@ -16,6 +14,6 @@ rm results/F_mimicry/* &> /dev/null
 rm results/FTC_mimicry/* &> /dev/null
 rm results/FT_gdkde/* &> /dev/null
 rm results/FT_mimicry/* &> /dev/null
-python mimicus_generate_attack_list.py $test_dir
-python reproduction/FT.py | tee "results_PDFrate_SVM.txt"
-#python reproduction/FTC.py | tee "results_PDFrate_RandomForest.txt"
+python $(dirname "$0")/mimicus_generate_attack_list.py $test_dir
+python $(dirname "$0")/reproduction/FT.py | tee "results_PDFrate_SVM.txt"
+#python $(dirname "$0")/reproduction/FTC.py | tee "results_PDFrate_RandomForest.txt"
--- tools/mimicus/setup_mimicus.sh
+++ tools/mimicus/setup_mimicus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Note: this script assumes that Python 2.7 and Pip are already installed, and that you are currently in the mimicus directory (where this script is located)
+# Note: this script assumes that Python 2.7 and Pip are already installed
 
-python setup.py develop --user
+python $(dirname "$0")/setup.py develop --user
 pip install --upgrade setuptools
 apt-get install curl perl
 pip install matplotlib
