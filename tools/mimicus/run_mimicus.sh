78cedcca32611b9d18705686aba6502204d89d76
GateGuy <gateguy33@gmail.com>
2020-11-24 22:26:37 -0500

    Updated readme
    Also fixed (very) minor bug in run script

--- tools/mimicus/run_mimicus.sh
+++ tools/mimicus/run_mimicus.sh
@@ -8,12 +8,12 @@ then
     exit
 fi
 
-rm results/FC_mimicry/* &> /dev/null
-rm results/F_gdkde/* &> /dev/null
-rm results/F_mimicry/* &> /dev/null
-rm results/FTC_mimicry/* &> /dev/null
-rm results/FT_gdkde/* &> /dev/null
-rm results/FT_mimicry/* &> /dev/null
+rm $(dirname "$0")/results/FC_mimicry/* &> /dev/null
+rm $(dirname "$0")/results/F_gdkde/* &> /dev/null
+rm $(dirname "$0")/results/F_mimicry/* &> /dev/null
+rm $(dirname "$0")/results/FTC_mimicry/* &> /dev/null
+rm $(dirname "$0")/results/FT_gdkde/* &> /dev/null
+rm $(dirname "$0")/results/FT_mimicry/* &> /dev/null
 python $(dirname "$0")/mimicus_generate_attack_list.py $test_dir
 python $(dirname "$0")/reproduction/FT.py | tee "results_PDFrate_SVM.txt"
 #python $(dirname "$0")/reproduction/FTC.py | tee "results_PDFrate_RandomForest.txt"
